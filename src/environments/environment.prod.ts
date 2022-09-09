f11a6c7ed1f7891511b84f3a4d35b7365c12402a
Jason <dljvaz@gmail.com>
2022-09-09 06:41:23 -0500

    .

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   BASE_API_URL: 'https://planetpups.herokuapp.com/api',
-  IMAGE_PATH: 'https://planetpups.herokuapp.com/tmp/images',
+  IMAGE_PATH: 'https://planetpups.herokuapp.com/tmp/images/',
 };
